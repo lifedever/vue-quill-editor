34c46683ca6c66e16c2d3d93b03122770b3e744c
gefangshuai <gefangshuai@163.com>
2020-07-10 00:07:33 +0800

    coding

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     outputDir: 'docs',
+    publicPath: process.env.NODE_ENV === 'production'
+        ? './'
+        : './',
     pages: {
         index: {
             // page 的入口
