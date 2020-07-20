2948338f48298b1f78b7a70f9f2043be7642422c
gefangshuai <gefangshuai@163.com>
2020-07-20 18:08:23 +0800

    coding

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 git add . && git commit -m 'coding'
-yarn version
 yarn build
 cp docs/vueQuillEditor.* ./lib/
 yarn demo
+yarn version
 git add . && git commit -m 'coding'
 git checkout master && git merge develop && git checkout develop
 git push --all && git push --tag
