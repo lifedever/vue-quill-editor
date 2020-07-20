0b0c8abc81b739319825f3447a9b348856235adc
gefangshuai <gefangshuai@163.com>
2020-07-20 17:32:01 +0800

    coding

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git add . && git commit -m 'coding'
-yarn demo
+yarn build
 yarn version
 git add . && git commit -m 'coding'
 git checkout master && git merge develop && git checkout develop
