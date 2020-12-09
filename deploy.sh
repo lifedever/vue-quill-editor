e3b49b64ce3f9915f2cb9612399977f5db8fd904
gefangshuai <gefangshuai@163.com>
2020-12-09 16:30:59 +0800

    coding

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@ cp docs/vueQuillEditor.* ./lib/
 yarn demo
 yarn version
 git add . && git commit -m 'coding'
-git checkout master && git merge develop -X theirs && git checkout develop
 git push --all && git push --tag
